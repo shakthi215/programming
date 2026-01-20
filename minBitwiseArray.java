c95b24373368ceae62a7f42e5c77db83358a490d
Shakthi S <shakthis045@gmail.com>
2026-01-20 18:51:48 +0530

    Day 2

---
+++ minBitwiseArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class minBitwiseArray {
+
+    public int[] minBitwiseArray1(List<Integer> nums) {
+        int[] result = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            int original = nums.get(i);
+            int candidate = -1;
+            for (int j = 1; j < original; j++) {
+                if ((j | (j + 1)) == original) {
+                    candidate = j;
+                    break;
+                }
+            }
+            result[i] = candidate;
+        }
+        return result;
+    }
+}

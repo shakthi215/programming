0d2f41b2ec64eb23c6c7e01b3e1c47e717782f04
Shakthi S <shakthis045@gmail.com>
2026-02-02 19:39:32 +0530

    mergearray

---
+++ mergearray.java
@@ -0,0 +1,38 @@
+class mergearray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m];
+        for (int i = 0; i < m; i++) {
+            temp[i] = nums1[i];
+        }
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (temp[i] <= nums2[j]) {
+                nums1[k++] = temp[i++];
+            } else {
+                nums1[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            nums1[k++] = temp[i++];
+        }
+
+        while (j < n) {
+            nums1[k++] = nums2[j++];
+        }
+    }
+}
+
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int j=m;
+//         for(int i=0;i<n;i++){
+//             nums1[j]=nums2[i];
+//             j++;
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }

b2d74296103c9424f3cfd60ad8d93937e4f03457
Shakthi S <shakthis045@gmail.com>
2026-02-05 10:22:07 +0530

    constructTransformedArray

---
+++ constructTransformedArray.java
@@ -0,0 +1,11 @@
+class constructTransformedArray {
+
+    public int[] constructTransformedArray1(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[(((i + nums[i]) % n) + n) % n];
+        }
+        return res;
+    }
+}

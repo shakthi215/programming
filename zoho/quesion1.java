14de7e08ba4a96bc513b460890d0a55972109455
Shakthi S <shakthis045@gmail.com>
2026-02-14 09:36:50 +0530

    question1

---
+++ zoho/quesion1.java
@@ -0,0 +1,48 @@
+package zoho;
+
+import java.util.Scanner;
+
+public class quesion1 {
+     public static void main(String acas[]){
+        Scanner s=new Scanner(System.in);
+        String text=s.next();
+        String pattern=s.next();
+        for(int i=0;i<text.length();i++){
+            int t=i;
+            int p=0;
+            while(t<text.length()&&p<pattern.length()){
+                char pc=pattern.charAt(p);
+                if(p+1<pattern.length()&&pattern.charAt(p+1)=='*'){
+                    char prev=pc;
+                    while(t<text.length()&&text.charAt(t)==prev){
+                        t++;
+                    }
+                    p+=2;
+                    
+                }else if(p+1<pattern.length()&&pattern.charAt(p+1)=='+'){
+                    char prev=pc;
+                    int count=0;
+                    while(t<text.length()&&text.charAt(t)==prev){
+                        t++;
+                        count++;
+                    }
+                    if(count==0)break;
+                    p+=2;
+
+                }else{
+                    if(t<text.length()&&text.charAt(t)==pc){
+                        t++;
+                        p++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            if(p==pattern.length()){
+                System.out.print(text.substring(i,t));
+                break;
+            }
+        }
+
+     } 
+}

e4ba648a162dd0c26355bbdf88b8678bfc593dfb
Shakthi S <shakthis045@gmail.com>
2026-03-01 11:33:55 +0530

    minPartitions

---
+++ minPartitions.java
@@ -0,0 +1,9 @@
+class minPartitions {
+    public int minPartitions1(String n) {
+        int ans = 0;
+        for (int i = 0; i < n.length(); ++i) {
+            ans = Math.max(ans, n.charAt(i) - '0');
+        }
+        return ans;
+    }
+}
